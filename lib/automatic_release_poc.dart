96cc69cfc19c4d5862271464e8cd68ff0e64abae
Enzo Lizama <lizama.enzo@gmail.com>
2022-01-13 12:15:53 -0500

    chore: addTwo method

--- lib/automatic_release_poc.dart
+++ lib/automatic_release_poc.dart
@@ -4,4 +4,7 @@ library automatic_release_poc;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  /// Returns [value] plus 2.
+  int addTwo(int value) => value + 2;
 }

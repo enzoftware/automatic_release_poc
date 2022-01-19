31b303c7dd8e1fc0a48cfc2baa5106fa05bc3222
Enzo Lizama <lizama.enzo@gmail.com>
2022-01-19 12:32:24 -0500

    chore: update

--- lib/automatic_release_poc.dart
+++ lib/automatic_release_poc.dart
@@ -2,9 +2,9 @@ library automatic_release_poc;
 
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 1.
+  /// Returns [value] plus 1
   int addOne(int value) => value + 1;
 
-  /// Returns [value] plus 2.
+  /// Returns [value] plus 2
   int addTwo(int value) => value + 2;
 }
